3f1ad7fd3510e025a93d936e03644d063f758beb
trelL <aegis.trellaren@gmail.com>
2020-03-08 12:33:48 -0500

    Initial Project

---
+++ bits_and_bobs/lib/coin_data.dart
@@ -0,0 +1,38 @@
+// Add imports
+
+const List<String> currenciesList = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
+];
+
+const List<String> cryptoList = [
+  'BTC',
+  'ETH',
+  'LTC',
+];
+
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+var apiKey; //initialize as const
+
+class CoinData {
+  //TODO Fill in this bitch.
+}
